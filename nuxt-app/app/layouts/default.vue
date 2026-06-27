aa22a583803513b90184856818537e96565d9f34
fcalderonj <fcaldera04@gmail.com>
2026-06-26 20:33:17 -0400

    correcciones layout y hero del index

--- nuxt-app/app/layouts/default.vue
+++ nuxt-app/app/layouts/default.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-    const route = useRoute()
-    const isActive = (to:String)=>route.path===to
+const route = useRoute()
+const isActive = (to: String) => route.path === to
 
-    const navigationItems=[
-    {label:"Inicio", to:"/"},
-    {label:"Registrarse", to:"/registrarse"},
-    {label:"Eventos", to:"/eventos"},
-    
+const navigationItems = [
+    { label: "Inicio", to: "/" },
+    { label: "Registrarse", to: "/registrarse" },
+    { label: "Eventos", to: "/eventos" },
+
 ]
 </script>
 
@@ -16,39 +16,42 @@
     <div class="min-h-screen bg-linear-to-br from-color-start-transition-bg to-color-end-transition-bg">
         <!-- header -->
         <header class="border-b backdrop-blur">
-            <div class="p-4 flex justify-around">
+            <div class="p-4 flex justify-between">
                 <a href="localhost:3000">
                     <!-- Titulo -->
-                <div>
-                    <p class="font-bold tracking-wide text-2xl">
-                        <span class=" text-navbar-accent">S</span class="text-p-title">mart <span class="text-navbar-accent">E</span>vent</p>
-                </div>
-                
+                    <div>
+                        <p class="font-bold tracking-wide text-2xl">
+                            <span class=" text-navbar-accent">S</span class="text-p-title">mart <span
+                                class="text-navbar-accent">E</span>vents
+                        </p>
+                    </div>
+
                 </a>
                 <!-- Páginas -->
-                 <div class="flex gap-3">
-                    <NuxtLink v-for="item in navigationItems" :key="item.to" :to="item.to" class=" py-1 px-2 text-sm font-medium "
-                    :class="isActive(item.to) ?
-                    'border-y-action-button border-b-2 shadow-sm' :
-                    ''">
+                <div class="flex gap-3">
+                    <NuxtLink v-for="item in navigationItems" :key="item.to" :to="item.to"
+                        class=" py-1 px-2 text-sm font-medium " :class="isActive(item.to) ?
+                            'border-y-action-button border-b-2 shadow-sm' :
+                            ''">
                         {{ item.label }}
 
                     </NuxtLink>
                     <UButton class="bg-action-button text-p-title rounded-2xl">Iniciar sesión</UButton>
-                 </div>
-                
+                </div>
+
             </div>
 
 
         </header>
 
 
-        <slot/>
+        <slot />
         <!-- footer -->
-        <footer>
+        <footer class=" bg-background-footer py-8">
             <div class="flex flex-col items-center">
-                <p class="font-bold tracking-wide text-2xl"><span class=" text-navbar-accent">S</span class="text-p-title">mart <span class="text-navbar-accent">E</span>vent</p>
-                <p>&reg 2026 Smart Events. Todos los derechos reservados</p>
+                <p class="font-bold tracking-wide text-2xl"><span class=" text-navbar-accent">S</span
+                        class="text-p-title">mart <span class="text-navbar-accent">E</span>vents</p>
+                <p class="text-footer-light-text">&reg 2026 Smart Events. Todos los derechos reservados</p>
             </div>
         </footer>
     </div>
--- nuxt-app/app/pages/index.vue
+++ nuxt-app/app/pages/index.vue
@@ -1,3 +1,25 @@
 <template>
-    
+    <div class=" min-h-screen bg-linear-to-br from-color-start-transition-bg to-color-end-transition-bg">
+        <section
+            class=" relative bg-linear-to-r from-start-transition-hero via-via-transition-hero to-end-transition-hero pt-24 pb-20">
+            <div class="max-w-7xl mx-auto px-6 grid md:grid-cols-2 gap-12 items-center">
+                <!--Texto-->
+                <div class="space-y-6">
+                    <h1 class="text-5xl md:text-6xl font-bold leading-tight">Conoce los mejores eventos pensados para ti
+                    </h1>
+                    <p class="text-lg md:text-xl text-gray-200 max-w-lg"> Navega y regístrate en cualquiera de los
+                        eventos que tenemos para la comunidad</p>
+                    <UButton
+                        class="px-8 py-4 rounded-full text-lg font-semibold flex items-center gap-2 bg-registrar-button hover:bg-action-button transition-colors text-white">
+                        Registrarse a
+                        un evento</UButton>
+                </div>
+
+            </div>
+
+
+        </section>
+
+
+    </div>
 </template>
