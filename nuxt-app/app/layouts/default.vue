9165deb7e6fb7b42ffb00937992409afa36d814a
Diego Álvarez <diegoalfonso.aa@gmail.com>
2026-06-29 22:37:02 -0400

    added user data as dropdown

--- nuxt-app/app/layouts/default.vue
+++ nuxt-app/app/layouts/default.vue
@@ -1,20 +1,45 @@
 <script setup lang="ts">
+import type { Usuario } from '~/types/usuario'
+
 const route = useRoute()
 const isActive = (to: String) => route.path === to
 
 const {user, clear}= useUserSession()
 
+const datosUsuario = user.value as Usuario | null
+
+const iniciales = computed(() => 
+    `${datosUsuario?.nombre?.[0] ?? ''}${datosUsuario?.apellido?.[0] ?? ''}`.toUpperCase()
+)
+
 const navigationItems = computed(()=>[
     { label: "Inicio", to: "/" },
     { label: "Registrarse", to: "/registrarse" },
     { label: "Eventos", to: "/eventos" },
-    ...(user.value ? [{label: "Registrar evento", to:"/registrarEvento"}, {label:"Administrar asistenes", to:"/administrarAsistente"}, {label:"Asministrar Staff", to:"/administrarStaff"}]:[])
+    ...(user.value ? [{label: "Registrar evento", to:"/registrarEvento"}, {label:"Administrar asistenes", to:"/administrarAsistente"}, {label:"Administrar Staff", to:"/administrarStaff"}]:[])
 
 ])
 
+const userMenuItems = [[
+    {label: `Usuario: ${datosUsuario?.nombre} ${datosUsuario?.apellido}`, type: 'label', icon:'i-lucide-user'},
+    {label: `Email: ${datosUsuario?.email}`, type: 'label',  icon:'mdi:at'},
+    {label: `Rol: ${datosUsuario?.rol}`, type: 'label', icon:'eos-icons:cluster-role'}
+    ],
+[
+    
+    {
+        label: 'Cerrar sesión',
+        icon: 'i-lucide-log-out',
+        onSelect: cerrarSesion
+    }
+]]
+
+
+
+
 async function cerrarSesion(){
     await clear()
-    await navigateTo("/index")
+    await navigateTo("/")
 }
 </script>
 
@@ -38,7 +63,7 @@ async function cerrarSesion(){
                 <!-- Páginas -->
                 <div class="flex gap-3">
                     <NuxtLink v-for="item in navigationItems" :key="item.to" :to="item.to"
-                        class=" py-1 px-2 text-sm font-medium " :class="isActive(item.to) ?
+                        class=" py-1 px-2 text-sm font-medium" :class="isActive(item.to) ?
                             'border-y-action-button border-b-2 shadow-sm' :
                             ''">
                         {{ item.label }}
@@ -47,10 +72,23 @@ async function cerrarSesion(){
                     <NuxtLink   to="/iniciarSesion">
                         <UButton v-if="!user" class="bg-action-button text-p-title rounded-2xl">Iniciar sesión</UButton>
                     </NuxtLink>
-                    <UButton v-if="user" @click="cerrarSesion" class="bg-action-button text-p-title rounded-2xl">Cerrar sesión</UButton>
+
+
 
                     
                 </div>
+                <div v-if="user" class="flex gap-4 ">
+
+                        <UButton  @click="cerrarSesion" class="bg-action-button text-p-title rounded-2xl">Cerrar sesión</UButton>
+                            <UDropdownMenu :items="userMenuItems" :content="{ align: 'end', sideOffset: 12 }"
+                            :ui="{ content: 'min-w-48', item: 'cursor-pointer' }" >
+                            <div class="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-registrar-button text-sm font-bold text-white transition-transform hover:scale-120"
+                                role="button" tabindex="0" aria-label="Abrir menú de usuario">
+                                {{iniciales}}
+                            </div>
+                        </UDropdownMenu>
+
+                    </div>
 
             </div>
 
